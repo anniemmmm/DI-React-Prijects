7bc681cd8c9aa65e4e0191ab6a5b521e63dd9662
anniemmmm <mamardashvilianna@gmail.com>
2023-03-19 20:02:35 +0400

    user card react

--- usercard_projreact/src/App.jsx
+++ usercard_projreact/src/App.jsx
@@ -29,7 +29,7 @@ function App(){
         <>
            <div className="containers">
                 <div className="input_container">
-                    <label>Ente Your Photo URL</label>
+                    <label className="label">Ente Your Photo URL</label>
                     <input
                     name="avatar"
                     placeholder="...Photo"
@@ -37,48 +37,51 @@ function App(){
                     onChange={(event)=>handleValue(event.target)}
                     />
                 </div>
-                <div className="input_container">
-                    <label>Enter Your First Name</label>
-                    <input
-                    placeholder="...Ani"
-                    name="firstName"
-                    value={user.FirstName}
-                    onChange={(e)=>handleValue(e.target)}
-                    />
+               <div className="containerss">
+                    <div className="input_container">
+                        <label className="label">Enter Your First Name</label>
+                        <input
+                        placeholder="...Ani"
+                        name="firstName"
+                        value={user.FirstName}
+                        onChange={(e)=>handleValue(e.target)}
+                        />
+                    </div>
+                    <div className="input_container">
+                        <label>Enter Your Last Name</label>
+                        <input
+                        placeholder="...colee"
+                        name="lastName"
+                        value={user.LastName}
+                        onChange={(e)=>handleValue(e.target)}
+                        />
+                    </div>
+                    <div className="input_container">
+                        <label className="label">Enter Your Phone Number</label>
+                        <input
+                        placeholder="+995 595 081 520"
+                        name="phoneNumber"
+                        value={user.PhoneNumber}
+                        onChange={(e)=>handleValue(e.target)}
+                        />
+                    </div>
+                    <div className="input_container">
+                        <label className="label">Enter Your City</label>
+                        <input
+                        placeholder="...Tskaltubo"
+                        name="City"
+                        value={user.City}
+                        onChange={(e)=>handleValue(e.target)}
+                        />
+                    </div>
+               </div>
+                <div className="buttons">
+                    <button onClick={()=>handleAdduser(user)}>Add User</button>
+                    <button onClick={()=>dituser(user)}>Edit User</button>
+                    <button onClick={()=>removeUser(user)}>Delete User</button>
                 </div>
-                <div className="input_container">
-                    <label>Enter Your Last Name</label>
-                    <input
-                    placeholder="...colee"
-                    name="lastName"
-                    value={user.LastName}
-                    onChange={(e)=>handleValue(e.target)}
-                    />
-                </div>
-                <div className="input_container">
-                    <label>Enter Your Phone Number</label>
-                    <input
-                    placeholder="+995 595 081 520"
-                    name="phoneNumber"
-                    value={user.PhoneNumber}
-                    onChange={(e)=>handleValue(e.target)}
-                    />
-                </div>
-                <div className="input_container">
-                    <label>Enter Your City</label>
-                    <input
-                    placeholder="...Tskaltubo"
-                    name="City"
-                    value={user.City}
-                    onChange={(e)=>handleValue(e.target)}
-                    />
-                </div>
-                <button onClick={()=>handleAdduser(user)}>Add User</button>
-                <button onClick={()=>editUser(user)}>Edit User</button>
-                <button onClick={()=>removeUser(user)}>Delete User</button>
-
-           </div>
-           <div className="flex-wrap"> {users.map((user)=>(
+            </div>
+            <div className="flex-wrap"> {users.map((user)=>(
                <div className="card">
                     <div className="card_header">
                         <img src="https://w7.pngwing.com/pngs/954/328/png-transparent-computer-icons-user-profile-avatar-heroes-head-recruiter-thumbnail.png" width="100px"height="100px"></img>
